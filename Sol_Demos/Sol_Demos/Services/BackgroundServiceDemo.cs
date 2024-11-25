e1d257c5573493b49b3e9d5444b69296e9e5c1da
Kishor Naik <kishor.naik011.net@gmail.com>
2024-11-25 10:20:02 +0530

    @KishorNaik : Init

---
+++ Sol_Demos/Sol_Demos/Services/BackgroundServiceDemo.cs
@@ -0,0 +1,29 @@
+﻿namespace Sol_Demos.Services;
+
+public class BackgroundServiceDemo : BackgroundService
+{
+    private readonly ILogger<BackgroundServiceDemo> _logger;
+
+    public BackgroundServiceDemo(ILogger<BackgroundServiceDemo> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("BackgroundService is starting.");
+
+        stoppingToken.Register(() =>
+            _logger.LogInformation("BackgroundService is stopping."));
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("BackgroundService is doing background work.");
+
+            // Simulate some background work
+            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+        }
+
+        _logger.LogInformation("BackgroundService has stopped.");
+    }
+}
